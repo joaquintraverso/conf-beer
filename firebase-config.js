a07e6532ae9b76de7cb907ae935daf714c917e8e
joaquintraverso <joaquintraverso03@gmail.com>
2022-11-24 20:47:55 -0300

    firebase key

--- firebase-config.js
+++ firebase-config.js
@@ -7,13 +7,6 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyDKW1rNELRAcFTM0zomRFfIV10ys662x3U",
-  authDomain: "conf-beer.firebaseapp.com",
-  projectId: "conf-beer",
-  storageBucket: "conf-beer.appspot.com",
-  messagingSenderId: "708791083024",
-  appId: "1:708791083024:web:498ad93263261f144daa95",
-  measurementId: "G-3JQ8TFKHE1"
 };
 
 // Initialize Firebase
